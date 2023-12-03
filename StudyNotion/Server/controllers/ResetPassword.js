f14994e1b7b64b02234bfc05d116c117030c1f1c
priyaK9157 <priyakumari2003004@gmail.com>
2023-12-03 10:41:10 +0530

    fial commit

--- StudyNotion/Server/controllers/ResetPassword.js
+++ StudyNotion/Server/controllers/ResetPassword.js
@@ -23,7 +23,7 @@ exports.resetPasswordToken = async (req, res) => {
 			},
 			{ new: true }
 		);
-		console.log("DETAILS", updatedDetails);
+		// console.log("DETAILS", updatedDetails);
 
 		const url = `https://study-notion-five.vercel.app/update-password/${token}`;
 
